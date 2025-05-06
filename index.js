7675b5b82bb88a309e9b828b05ff834f83427e99
River <river.erhardt@outlook.com>
2025-05-06 16:26:10 -0700

    username change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 // Replace with your Azure DB info:
 const db = mysql.createPool({
   host: 'truck-db.mysql.database.azure.com',
-  user: 'capuser@truck-db',
+  user: 'capuser',
   password: process.env.DB_PASSWORD,
   database: 'food_trucks',
   port: 3306,
