8b4e2d1997e3d2eff7c8ce29ab4c9bb9211a8f2a
River <river.erhardt@outlook.com>
2025-05-05 23:56:26 -0700

    "force user to look at index"

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const path = require('path');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Replace with your Azure DB info:
 const db = mysql.createConnection({
@@ -12,9 +20,6 @@ const db = mysql.createConnection({
   password: 'projPass1!',
   database: 'food_trucks'
 });
-
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/trucks', (req, res) => {
   db.query('SELECT * FROM trucks', (err, results) => {
