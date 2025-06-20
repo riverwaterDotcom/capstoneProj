fd127927e29707c31066ca50c6814552ea865013
River <river.erhardt@outlook.com>
2025-06-20 13:46:43 -0700

    database move/subscrip changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 // Azure DB info:
 const db = mysql.createPool({
-  host: 'truck-db.mysql.database.azure.com',
+  host: 'cap-proj-db.mysql.database.azure.com',
   user: 'capuser',
   password: process.env.DB_PASSWORD,
   database: 'food_trucks',
